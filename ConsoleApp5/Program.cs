10658fa6d485a0afff716b5d72aef24f7884f12b
wand <wand@DESKTOP-4FS8GTI>
2023-01-17 16:09:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Random rnd = new Random();
+int rnd_damage = rnd.Next(750,2000);
+
+Plane uiobi = new Plane("Never", 1250, rnd_damage);
+uiobi.War();
---
+++ ConsoleApp5/Simple.cs
@@ -0,0 +1,56 @@
+﻿class Car
+{
+    public string name;
+    public int id;
+    public int date;
+    
+    public Car(string name, int id, int date)
+    {
+        this.name = name;
+        this.id = id;
+        this.date = date;
+    }
+    public void Print() => Console.WriteLine($"Имя машины: {name}\nСпециальный номер машины: {id}\nДата выпуска машины: {date}\n");
+    
+}
+class Plane
+{
+    public string name;
+    public int health;
+    public int damage;
+    
+
+    public Plane(string name, int health, int damage)
+    {
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
+    }
+
+    public void Shot()
+    {
+        Console.WriteLine($"Самолет {name} нанёс {damage} урона.");
+    }
+
+    public void War()
+    {
+        int lastHP = health - damage;
+        
+        if(damage > 1800)
+        {
+            Console.WriteLine($"КРИТИЧЕСКИЙ УРОН {damage}. Вы убили врага!");
+            
+        }
+        else if(damage < 1800)
+        {
+            if (lastHP > 0)
+            {
+                Console.WriteLine($"Вы выстрелили на {damage} урона. У врага осталось {lastHP}");
+            }
+            else if (lastHP < 0)
+            {
+                Console.WriteLine($"Вы убили врага! Ваш урон был {damage}");
+            }
+        }
+    }
+}
